0d4df8a30569145f8d6a658385c84faf4b4a4351
Roberto De Oliveira Pinheiro <robertopinheiro@id.uff.br>
2026-02-18 15:40:31 -0300

    Add Footer component with copyright notice

---
+++ src/components/Clientes.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2026 ROP Consultoria TI. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
